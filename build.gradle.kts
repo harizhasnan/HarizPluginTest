0dfdb4ef89bfeadd14b0fd6ef064cb7a14fdf6d2
harizhasnan <hariz.hasnan@gmail.com>
2023-12-23 11:24:47 +0800

    First Commit my PhD Interview Task - Hariz Hasnan

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     id("java")
+    //id("org.jetbrains.kotlin.jvm' version '1.5.21')
 }
 
+tasks.named<Jar>("jar"){
+    manifest {
+        attributes["Main-Class"] = "com.GPT2inferencePlugin"
+    }
+}
 group = "org.apache.httpcomponents"
 version = "1.0-SNAPSHOT"
 
@@ -12,6 +18,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.apache.httpcomponents:httpclient:4.5.13")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+
 }
 
 tasks.test {
--- src/main/java/com/GPT2main/UIScreen.java
+++ src/main/java/com/GPT2main/UIScreen.java
@@ -1,2 +1,22 @@
-package com.GPT2main;public class UIScreen {
+package com.GPT2main;
+
+import javax.swing.*;
+
+public class UIScreen extends JFrame{
+    private JPanel mainPanel;
+
+    public UIScreen(){
+        add(mainPanel);
+
+        setTitle("Your GPT");
+        setSize(650,500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+    public static void main(String[] args){
+        UIScreen myScreen = new UIScreen();
+    }
+
 }
